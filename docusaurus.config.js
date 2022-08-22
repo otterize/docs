51ed7e72a202c007c974d86e1e360c7acbc1f242
Tomer Greenwald <tomer@otterize.com>
2022-08-22 16:01:42 +0300

    Updated logo and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Otterize',
+        title: '',
         logo: {
           alt: 'Otterize Logo',
           src: 'img/logo.svg',
