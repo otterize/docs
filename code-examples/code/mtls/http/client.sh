903247c67182a1a8b0f479b2707bc66cc20b4eeb
otterizebot <otterizebot@users.noreply.github.com>
2022-09-10 16:26:21 +0000

    deploy: 8e771f058fe021f97e044bd0f55d6fa28bc82532

---
+++ code-examples/code/mtls/http/client.sh
@@ -0,0 +1,3 @@
+curl  --cert /var/otterize/credentials/svid.pem \
+      --key /var/otterize/credentials/key.pem \
+      --cacert /var/otterize/credentials/bundle.pem https://server.otterize-tutorial-mtls/hello
---
+++ code-examples/code/mtls/http/server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(port=443,
+            ssl_context=(
+                '/var/otterize/credentials/svid.pem',
+                '/var/otterize/credentials/key.pem'))
