bf9a47d0265b5f51e6ab7d6b9c14104fb5534810
Ori Shoshan <ori@otterize.com>
2022-09-22 15:06:26 +0300

    Docs for charts and uninstall

--- static/code-examples/code/mtls/kafka/client.js
+++ static/code-examples/code/mtls/kafka/client.js
@@ -4,7 +4,6 @@ const {Kafka} = require('kafkajs')
 const kafka = new Kafka({
     brokers: ['kafka.kafka:9092'],
     ssl: {
-        rejectUnauthorized: false,
         ca: [fs.readFileSync('/var/otterize/credentials/bundle.pem', 'utf-8')],
         key: fs.readFileSync('/var/otterize/credentials/key.pem', 'utf-8'),
         cert: fs.readFileSync('/var/otterize/credentials/svid.pem', 'utf-8')
