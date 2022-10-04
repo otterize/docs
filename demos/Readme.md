325ab70b44188602600b2391de400df800afb4c5
Tomer Greenwald <tomerg@gmail.com>
2022-10-04 13:22:05 +0300

    Updated demo recorder

--- demos/Readme.md
+++ demos/Readme.md
@@ -2,5 +2,6 @@ To record a demo
 
 # Install asciinema
 brew install asciinema
+brew install pv
 # Run
 asciinema rec -c ./demo-network-policies.sh
--- demos/demo-network-policies.sh
+++ demos/demo-network-policies.sh
@@ -19,47 +19,58 @@ pei "# Let's play with network policies"
 prompt
 pei "# We'll start by deploying"
 prompt
-pei "# - client and server pods communicating over HTTP"
-prompt
-pei "# - a default deny network policy to block the communication between them"
+pei "# - client, client-other and server pods communicating over HTTP"
 prompt
 pei "kubectl apply -f https://docs.otterize.com/code-examples/automate-network-policies/all.yaml"
 prompt
 pei ""
 prompt
+sleep 2
 pei "# Check that the client and server pods were deployed"
 prompt
 pei "kubectl get pods -n otterize-tutorial-npol"
-prompt
-pei "# Check that the default deny network policy was deployed"
-prompt
-pei "kubectl get NetworkPolicies -n otterize-tutorial-npol"
 prompt
 pei ""
 prompt
 pei "# Let's look at the client's output logs"
 prompt
-pei "kubectl logs --tail -0 -f -n otterize-tutorial-npol deploy/client | head -n 4"
+pei "kubectl logs --tail -0 -f -n otterize-tutorial-npol deploy/client | head -n 13"
 prompt
-pei "# We can see that it's timing out when calling the server"
+pei "# We can see that it's able to call the server"
 prompt
-pei "# because of the default deny network policy"
+pei "# Let's look at the client-other's output logs"
+prompt
+pei "kubectl logs --tail -0 -f -n otterize-tutorial-npol deploy/client-other | head -n 13"
+prompt
+pei "# We can see that it too is able to call the server"
+prompt
+pei ""
+prompt
+pei "# This is our base mode - both clients are able to call the server."
+prompt
+pei ""
 prompt
 pei "# Let's apply an intents file which Otterize will use"
 prompt
-pei "# to automatically configure network policies"
+pei "# to automatically configure network policies allowing calls"
+prompt
+pei "# from the client to the server while blocking all other unintended calls."
 prompt
 pei "# The intents file is a custom resource you can apply directly to your K8s cluster"
 prompt
 pei "# It looks as follows"
 prompt
-pei "curl https://docs.otterize.com/code-examples/automate-network-policies/intents/intents.yaml"
+pei "curl https://docs.otterize.com/code-examples/automate-network-policies/intents.yaml"
 echo # the intents yaml doesn't have new line at the end of the file
 prompt
-pei "kubectl apply -f https://docs.otterize.com/code-examples/automate-network-policies/intents/intents.yaml"
+pei "kubectl apply -f https://docs.otterize.com/code-examples/automate-network-policies/intents.yaml"
 prompt
-pei "# And we can now see that the client is able to call the server"
+pei "# And we can now see that the client is still able to call the server"
 prompt
-pei "kubectl logs --tail 0 -f -n otterize-tutorial-npol deploy/client | head -n 18"
+pei "kubectl logs --tail 0 -f -n otterize-tutorial-npol deploy/client | head -n 13"
+prompt
+pei "# While the client-other pod is timing out when calling the server"
+prompt
+pei "kubectl logs --tail 0 -f -n otterize-tutorial-npol deploy/client-other | head -n 4"
 prompt
 pei "# That's how easy it is to manage network policies with Otterize 🦦"
