25a5261b8b17fc787b43a45520c24f939c6cb1ee
Tomer Greenwald <tomerg@gmail.com>
2022-10-11 14:42:53 +0300

    Set color mode to light only

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -139,6 +139,11 @@ const config = {
                 theme: lightCodeTheme,
                 darkTheme: darkCodeTheme,
             },
+            colorMode: {
+                defaultMode: 'light',
+                disableSwitch: true,
+                respectPrefersColorScheme: false,
+            },
         }),
 };
 
