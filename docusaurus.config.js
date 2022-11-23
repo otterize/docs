f3d7066fce29b3225a45bc4bf74f9a555cf565f6
Tomer Greenwald <tomer@otterize.com>
2022-11-23 21:49:55 +0200

    Set trailingSlash: false (#16)
    * Set trailingSlash to false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,6 +12,7 @@ const config = {
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.svg",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
