12fe429c2c2798e3a3cd7af721ef80a1bc92e38e
Ori Shoshan <ori@otterize.com>
2023-10-11 16:25:45 +0200

    Add verification for broken @includes statements

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,7 @@
 
 const lightCodeTheme = require("prism-react-renderer/themes/oceanicNext");
 const darkCodeTheme = require("prism-react-renderer/themes/oceanicNext");
+const path = require("path");
 
 // /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -31,6 +32,7 @@ const config = {
     { src: "/pixels.js", async: true },
   ],
   plugins: [
+    path.resolve(__dirname, 'plugins', 'verify-includes'),
     ['@docusaurus/plugin-client-redirects',
     {
       redirects: [
---
+++ plugins/verify-includes/index.js
@@ -0,0 +1,30 @@
+const spawnSync = require('child_process').spawnSync;
+
+module.exports = function (context, options) {
+  return {
+    name: 'verify-includes',
+    async postBuild({siteConfig = {}, routesPaths = [], outDir}) {
+      const child = spawnSync('grep',
+          // -l - only print filenames
+          // -R - recursive
+          ['-lR',
+            'include file not found',
+            outDir], {
+        encoding: 'utf-8',
+        stdio: [process.stdin, 'pipe', process.stderr]
+      });
+      // Exit codes:
+      //      The grep utility exits with one of the following values:
+      //
+      //      0     One or more lines were selected.
+      //      1     No lines were selected.
+      //      >1    An error occurred.
+      // So we expect 1 - no lines were selected, and no errors.
+      if (child.status !== 1) {
+        console.error("Found missing includes in these files:")
+        console.error(child.stdout);
+        process.exit(child.status);
+      }
+    },
+  };
+};
