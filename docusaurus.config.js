7c69c55b03886484387bb5ce2947d1f94de11c8b
Amit Mittal <mittalmailbox@gmail.com>
2023-10-23 00:38:47 +0530

    Enable dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -249,8 +249,8 @@ const config = {
       },
       colorMode: {
         defaultMode: "light",
-        disableSwitch: true,
-        respectPrefersColorScheme: false,
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
       },
     }),
 };
